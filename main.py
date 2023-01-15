5adf68a22c0113415d90bf02ddb0782fa7f89fc5
SauquetAlex <54484828+SauquetAlex@users.noreply.github.com>
2023-01-15 12:33:28 -0800

    Auto cropping and RickRoll

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import cv2
 import numpy as np
 
-# Write the name of the video file here (must be in the same folder as the script)
-cap = cv2.VideoCapture('CHARGED UP presented by Haas Teaser.mp4')
-
+# Write the name of the video file here (must be in the same folder as the script) and its FPS
+cap = cv2.VideoCapture('Rick.mp4')
+fps = 30
 
 # LED Matrix size
 dim = (22, 22)
@@ -12,11 +12,36 @@ dim = (22, 22)
 # Output file
 f = open("frames.txt", "w+")
 
+# Imported code from https://stackoverflow.com/questions/13538748/crop-black-edges-with-opencv
+
+
+def autocrop(image, threshold=0):
+    """Crops any edges below or equal to threshold
+    Crops blank image to 1x1.
+    Returns cropped image.
+    """
+    if len(image.shape) == 3:
+        flatImage = np.max(image, 2)
+    else:
+        flatImage = image
+    assert len(flatImage.shape) == 2
+
+    rows = np.where(np.max(flatImage, 0) > threshold)[0]
+    if rows.size:
+        cols = np.where(np.max(flatImage, 1) > threshold)[0]
+        image = image[cols[0]: cols[-1] + 1, rows[0]: rows[-1] + 1]
+    else:
+        image = image[:1, :1]
+    return image
+
+
 while (cap.isOpened()):
     ret, frame = cap.read()
-    frameRGB = []
     if ret == False:
         break
+    cv2.imshow('Original', frame)
+    # If there are black borders, crop them here, otherwise comment this out
+    frame = autocrop(frame, 50)
     image = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
     f.write("{")
     for i in range(0, dim[0]):
@@ -28,11 +53,11 @@ while (cap.isOpened()):
     f.write("}")
     f.write("\n")
     image = cv2.resize(image, (500, 500), interpolation=cv2.INTER_AREA)
-    cv2.imshow('image', image)
-    if cv2.waitKey(30) & 0xFF == ord('q'):
+    cv2.imshow('Video2', image)
+    if cv2.waitKey(1000//fps) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
 
 
-# Made by Alexandre Sauquet :)
+# AS
